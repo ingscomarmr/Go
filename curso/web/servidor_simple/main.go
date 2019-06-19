0440abddaa13ec5f51c8834e954e3fd1aa31d1bf
Omar Mr <ingscomarmr@gmail.com>
2019-06-18 22:12:52 -0500

    Se agrego un multiplexor para mayor poder en las peticiones http

--- curso/web/servidor_simple/main.go
+++ curso/web/servidor_simple/main.go
@@ -9,8 +9,17 @@ func main() {
 
 	dirPublic := "www"
 	dirHost := "localhost:8080"
-	http.Handle("/", http.FileServer(http.Dir(dirPublic)))
-	fmt.Printf("Server lanzado:%s carpeta:%s\n", dirHost, dirPublic)
-	fmt.Println(http.ListenAndServe(dirHost, nil))
+
+	//forma tradicional pero limitada de lanzar un servidor
+	//http.Handle("/", http.FileServer(http.Dir(dirPublic))) //agregar una carpeta que sera publica
+	//fmt.Printf("Server lanzado:%s carpeta:%s\n", dirHost, dirPublic)
+	//fmt.Println(http.ListenAndServe(dirHost, nil)) //lansar el servidor para que este escuchando
+
+	//lanzar un servidor web por medio de un multiplexor, es mas dinamico y robusto que un servidor simple
+	mux := http.NewServeMux()                  //declaramos el multiplexo
+	fs := http.FileServer(http.Dir(dirPublic)) //declaramos la ruta de donde servira
+	mux.Handle("/", fs)                        //indicamos si contiene alguna ruta web especifica
+	fmt.Println("Iniciar servicio web en ", dirPublic)
+	fmt.Println(http.ListenAndServe(dirHost, mux)) //iniciamos el server
 
 }
